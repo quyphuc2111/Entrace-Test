a75cbd595053098efaace4d0b008b3576068b025
QUYYPHUCC\ASUS <quyphuc211120@gmail.com>
2020-09-17 11:50:43 +0700

    no message

--- Problem Solving/b2.js
+++ Problem Solving/b2.js
@@ -11,16 +11,16 @@ for (let i = 0; i < array2.length; i++) {
     result2.push(array2.slice(i, i + 1))
 }
 
-
+//case1
 for (let i = 0; i < result1.length; i++) {
     for(let j =0;j< result2.length;j++) {
         if(i==j) {
             result.push(result1[i] + result2[i])
         }
     }
-   
-    
 }
+//case2
+
 console.log(result.join(''))
 
 
